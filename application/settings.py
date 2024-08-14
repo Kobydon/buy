f0dc3aebbdd18d1ee178bcdd408bb2c7d1dc4d88
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-08-14 21:02:46 +0000

    ssj

--- application/settings.py
+++ application/settings.py
@@ -14,7 +14,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'jxkalmhefacbuk@gmail.com'
-app.config['MAIL_PASSWORD'] = 'dfilwxnwpcalytdv'
+app.config['MAIL_PASSWORD'] = 'qhsf mguh pzuh dcmx'
 
 
 """
